5db3fc3fdfdeca3c9efb68f133bef66be49a1976
mykolasereda <nickolaysereda@gmai.com>
2024-02-16 17:57:07 +0100

    onSubmit add

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -5,8 +5,22 @@ const AuthForm = () => {
 
     const [isLogIn, setIsLogin] = useState(true);
 
+
+
+     // Event handler for form submission
+  const handleSubmit = (event) => {
+    // The preventDefault method is a function that belongs to the event object passed 
+    // to event handlers in JavaScript. 
+    // In the context of a form submission, preventDefault 
+    // is used to stop the default behavior of the browser, 
+    // which is to refresh the page or navigate to a new URL when the form is submitted.
+    event.preventDefault();
+    // Perform necessary actions, such as sending data to a server
+
+  };
+
     return (
-        <form method='POST' className={classes.form}>
+        <form onSubmit={handleSubmit} className={classes.form}>
             <h1>
                 {isLogIn ? 'Log in' : 'Sign Up'}
             </h1>
