85956aa91323da042e7a90882a45c8bd3272eb09
mykolasereda <nickolaysereda@gmai.com>
2024-02-20 13:54:16 +0100

    Router add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import Root from './pages/Root.jsx';
+import ShopPage from './pages/ShopPage.jsx';
+import AuthPage from './pages/AuthPage.jsx';
+import CartPage from './pages/CartPage.jsx';
+import ItemDetailsPage from './pages/ItemDetailsPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,  
+    children: [
+      // index: true means this will be first route of parent by default
+      {
+        index: true, element: <ShopPage />,
+      },
+      {
+        path: 'auth',
+        element: <AuthPage />,
+      },
+      {
+        path: 'cart',
+        element: <CartPage />,
+      },
+      // Should be last otherwise prev routes won't work
+      {
+        path: ':productId',
+        element: <ItemDetailsPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
+
   return (
-    <Root />
+    // <Root />
+    <RouterProvider router={router} />
   );
 }
 
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,12 +1,13 @@
 
 import AppHeader from '../components/AppHeader.jsx';
+import { Outlet } from 'react-router-dom';
 
 function Root() {
     return (
         <>
             <AppHeader />
             <main>
-              
+                <Outlet />
             </main>
         </>
     );
